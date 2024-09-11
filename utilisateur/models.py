9a353b77f59d6da26cedca2e4993ba0ccb6f04e5
vousetes <traoreahmed937@gmail.com>
2024-09-11 00:48:59 -0400

    Creation du model user avec ses migrations

---
+++ utilisateur/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .manager import utilisateurManager
+
+
+
+UTILISATEUR_TYPES_CHOICES =(
+    ("adminitration_menbre","Menbre d'administration"),
+    ("Etudiant", "Etudiant"),
+    ("Enseignant", "Enseignant"),
+    ("Parent", "Parent"),
+
+)
+
+
+# Create your models here.
+
+
+class utilisateur(AbstractBaseUser, PermissionsMixin):
+    nom = models.CharField(_("Nom"),max_length=255)
+    prenom = models.CharField(_("prenom"),max_length=255)
+    email = models.EmailField(_("Couriel Personnel"),unique=True)
+    type_utilisateur = models.CharField(max_length=255, choices=UTILISATEUR_TYPES_CHOICES,default="etudiant")
+    is_active = models.BooleanField(_("Compte Actif"),default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = utilisateurManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = []
+
+    class Meta:
+        verbose_name = _("Utilisateur")
+        verbose_name_plural = _("Utilisateurs")
